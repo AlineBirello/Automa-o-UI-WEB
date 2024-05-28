e8dc5241ed2a106418ddb865bb47888dd111b22d
AlineBirello <aline.birello@gmail.com>
2024-05-28 14:01:41 -0300

    4.5.1AutomacaoUI

--- Cypress/e2e/2 -AutomacaoAPI.cy.js
+++ Cypress/e2e/2 -AutomacaoAPI.cy.js
@@ -1,6 +1,6 @@
 ///<reference types= "cypress"/>
 
-import url_api from"../../fixtures/url_api.json";
+import url_api from"../fixtures/url_api.json";
 
 describe('Funcionalidade: Produtos', () => {
 
@@ -10,7 +10,7 @@ describe('Funcionalidade: Produtos', () => {
 
     it('Cadastrar Coupons de desconto com 10% - POST', () => {
         let token = "YWRtaW5fZWJhYzpAYWRtaW4hJmJAYyEyMDIy"
-        cy.api({
+        cy.request({
             method: 'POST',
             url: url_api.urlCoupons,
             headers: {authorization: token},
@@ -30,7 +30,7 @@ describe('Funcionalidade: Produtos', () => {
 
     it('Listar Coupons cadastrado com letras e numeros - GET', () => {
         let token = "YWRtaW5fZWJhYzpAYWRtaW4hJmJAYyEyMDIy"
-        cy.api({
+        cy.request({
             method: 'GET',
             url: url_api.urlCouponsId,
             headers: {authorization: token},
@@ -49,27 +49,4 @@ describe('Funcionalidade: Produtos', () => {
 
         })
     })    
-
-    it('Utilizar Coupons já existente - POST', () => {
-        it('Cadastrar Coupons- POST', () => {
-            let token = "YWRtaW5fZWJhYzpAYWRtaW4hJmJAYyEyMDIy"
-            cy.api({
-                method: 'POST',
-                url: url_api.urlCouponsId,
-                headers: {authorization: token},
-                body: {
-                    "code": "Ganhe10",
-                    "amount": "10",
-                    "discount_type": "fixed_product",
-                    "description": "Cupom de desconto de teste", 
-
-                
-                    },failOnStatusCode: false
-               
-            }).should((response) => {
-                expect(response.status).equal(400)
-                expect(response.body.message).equal("O código de cupom já existe")
-            })
         })    
-    })
-})
